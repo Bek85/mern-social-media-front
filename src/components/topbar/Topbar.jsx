66f9de74ee611d87a9b309b073377f38afe53500
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-10 18:07:37 +0500

    create Topbar component

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,44 @@
+import classes from './topbar.module.scss';
+import { MdChat, MdNotifications, MdPerson, MdSearch } from 'react-icons/md';
+import person from '../../assets/person/1.jpeg';
+
+export default function Topbar() {
+  return (
+    <div className={classes.topbarContainer}>
+      <div className={classes.topbarLeft}>
+        <span className={classes.logo}>Social App</span>
+      </div>
+      <div className={classes.topbarCenter}>
+        <div className={classes.searchbox}>
+          <MdSearch />
+          <input
+            type="text"
+            className={classes.searchInput}
+            placeholder="Search for friends, posts or videos"
+          />
+        </div>
+      </div>
+      <div className={classes.topbarRight}>
+        <div className={classes.topbarLinks}>
+          <span className={classes.topbarLink}>Homepage</span>
+          <span className={classes.topbarLink}>Timeline</span>
+        </div>
+        <div className={classes.topbarIcons}>
+          <div className={classes.topbarIconItem}>
+            <MdPerson />
+            <span className={classes.topbarIconBadge}>1</span>
+          </div>
+          <div className={classes.topbarIconItem}>
+            <MdChat />
+            <span className={classes.topbarIconBadge}>2</span>
+          </div>
+          <div className={classes.topbarIconItem}>
+            <MdNotifications />
+            <span className={classes.topbarIconBadge}>5</span>
+          </div>
+        </div>
+        <img src={person} alt="" className={classes.topbarImg} />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import Topbar from '../../components/topbar/Topbar';
+import classes from './home.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+}
