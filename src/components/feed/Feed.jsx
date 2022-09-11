c245ba0f13b10495a26be8601e30ba2671fe83aa
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-11 10:41:24 +0500

    create markup and styling for <Share/> component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,5 +1,12 @@
+import Share from '../share/Share';
 import classes from './feed.module.scss';
 
 export default function Feed() {
-  return <div className={classes.feed}>Feed</div>;
+  return (
+    <div className={classes.feed}>
+      <div className={classes.feedWrapper}>
+        <Share />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/share/Share.jsx
@@ -0,0 +1,43 @@
+import classes from './share.module.scss';
+import person from '../../assets/person/1.jpeg';
+import { MdEmojiEmotions, MdLabel, MdPermMedia, MdRoom } from 'react-icons/md';
+
+export default function Share() {
+  return (
+    <div className={classes.shareContainer}>
+      <div className={classes.shareWrapper}>
+        <div className={classes.shareTop}>
+          <img className={classes.shareProfileImg} src={person} alt="" />
+          <input
+            className={classes.shareInput}
+            type="text"
+            placeholder="What's in your mind"
+          />
+        </div>
+        <hr className={classes.shareHr} />
+
+        <div className={classes.shareBottom}>
+          <div className={classes.shareOptions}>
+            <div className={classes.shareOption}>
+              <MdPermMedia className={classes.red} />
+              <span className={classes.shareOptionText}>Photo or Video</span>
+            </div>
+            <div className={classes.shareOption}>
+              <MdLabel className={classes.blue} />
+              <span className={classes.shareOptionText}>Tag</span>
+            </div>
+            <div className={classes.shareOption}>
+              <MdRoom className={classes.green} />
+              <span className={classes.shareOptionText}>Location</span>
+            </div>
+            <div className={classes.shareOption}>
+              <MdEmojiEmotions className={classes.yellow} />
+              <span className={classes.shareOptionText}>Feelings</span>
+            </div>
+          </div>
+          <button className={classes.shareBtn}>Share</button>
+        </div>
+      </div>
+    </div>
+  );
+}
