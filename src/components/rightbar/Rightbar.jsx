15e9d0f685e1ec25ce0f836af8b3c3da0b7b0e43
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-11 16:41:10 +0500

    create and style <Rightbar/> component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,51 @@
 import classes from './rightbar.module.scss';
+import gift from '../../assets/gift.png';
+import ad from '../../assets/ad.png';
+import person3 from '../../assets/person/3.jpeg';
 
 export default function Rightbar() {
-  return <div className={classes.rightbar}>Rightbar</div>;
+  return (
+    <div className={classes.rightbar}>
+      <div className={classes.rightbarWrapper}>
+        <div className={classes.birthdayContainer}>
+          <img className={classes.birthdayImg} src={gift} alt="" />
+          <span className={classes.birthdayText}>
+            <b>Ben Jackson</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+        <img className={classes.rightbarAd} src={ad} alt="" />
+        <h4 className={classes.rightbarTitle}>Online Friends</h4>
+        <ul className={classes.friendList}>
+          <li className={classes.rightbarFriend}>
+            <div className={classes.rightbarImgContainer}>
+              <img src={person3} alt="" />
+              <span className={classes.rightbarOnline}></span>
+            </div>
+            <span>John Doe</span>
+          </li>
+          <li className={classes.rightbarFriend}>
+            <div className={classes.rightbarImgContainer}>
+              <img src={person3} alt="" />
+              <span className={classes.rightbarOnline}></span>
+            </div>
+            <span>John Doe</span>
+          </li>
+          <li className={classes.rightbarFriend}>
+            <div className={classes.rightbarImgContainer}>
+              <img src={person3} alt="" />
+              <span className={classes.rightbarOnline}></span>
+            </div>
+            <span>John Doe</span>
+          </li>
+          <li className={classes.rightbarFriend}>
+            <div className={classes.rightbarImgContainer}>
+              <img src={person3} alt="" />
+              <span className={classes.rightbarOnline}></span>
+            </div>
+            <span>John Doe</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
