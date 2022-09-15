d3ab23092dd89b7b5f920aa1431ecbadcbff4b46
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-15 21:20:02 +0500

    implemenet register flow

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,35 @@
-import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import classes from './register.module.scss';
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordRepeat = useRef();
+  const redirect = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (passwordRepeat.current.value !== password.current.value) {
+      password.current.setCustomValidity("Passwords don't match");
+    } else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      };
+
+      try {
+        await axios.post('/api/auth/register', user);
+        redirect('/login');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className={classes.registerContainer}>
       <div className={classes.registerWrapper}>
@@ -12,11 +40,21 @@ export default function Register() {
           </span>
         </div>
         <div className={classes.registerRight}>
-          <form className={classes.registerBox}>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Repeat your password" />
+          <form onSubmit={handleSubmit} className={classes.registerBox}>
+            <input type="text" placeholder="Username" ref={username} required />
+            <input type="email" placeholder="Email" ref={email} required />
+            <input
+              type="password"
+              placeholder="Password"
+              ref={password}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Repeat your password"
+              ref={passwordRepeat}
+              required
+            />
             <button type="submit" className={classes.registerBtn}>
               Sign Up
             </button>
