d6c03470c0d48c9c1171a0e51424b152ad4a1abd
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-18 13:31:04 +0500

    sort posts by its "createdAt" property

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -15,7 +15,11 @@ export default function Feed({ username }) {
       const response = username
         ? await axios.get(`/api/posts/profile/${username}`)
         : await axios.get(`/api/posts/timeline/${user._id}`);
-      setPosts(response.data);
+      setPosts(
+        response.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
     };
     fetchPosts();
   }, [username, user._id]);
